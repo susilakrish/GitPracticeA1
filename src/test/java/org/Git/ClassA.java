5fb7a4c4605fd1205e616b92c030189c37c92cd2
susilakrish <susilakrish2096@gmail.com>
2024-09-04 17:54:19 +0530

    Tester2

--- src/test/java/org/Git/ClassA.java
+++ src/test/java/org/Git/ClassA.java
@@ -4,4 +4,12 @@ public class ClassA {
 	public void fruits() {
 		System.out.println("Apple");
 	}
+	public void orange() {
+System.out.println("Orange");
+	}
+	
+	public void cucumber() {
+System.out.println("cucumber");
+	}
 }
+
